759ce0e8ade12eb069cd03a3755f64b1afd386f8
“Caleb <calebh27@byu.edu>
2022-03-04 09:47:21 -0700

    Complete reverse_text.py file

--- reverse_text.py
+++ reverse_text.py
@@ -1,27 +1,83 @@
-"""
-With this code, punctuation is not flipped. So if you had the sentence 
-"I have a dog, his name is Buddy", it would be flipped to be 
-"Buddy is name his dog, a have I"
-""" 
+"""Functions for reversing text files"""
 
-# Open the file in write mode
-f2 = open("output.txt", "w")
-  
-data = []
+import string 
 
-# Open the input file again and get 
-# the content as list to a variable data
-with open("input.txt", "r") as myfile:
-    for line in myfile:
-        # reading each word        
-        for word in line.split():
-            # add the words           
-            data.append(word + " ") 
-  
-# reverse the array
-data_2 = data[::-1]
-  
-# write the reversed list to the output file
-f2.writelines(data_2)
-  
-f2.close()
+def reverse_text(input_file_string, output_file_string, newlines=True):
+    """Takes text input file and creates a new file with all the text flipped, NOT including punctuation. 
+    For example, "Hi, I'm John" -> "John I'm Hi,"
+
+    Args:
+        input_file_string (str): Path to input file
+        output_file_string (str): Path to the output file. 
+        newlines (bool, optional): Whether or not you want the output file to have new lines. Defaults to True.
+    """
+    output_file = open(output_file_string, "w")
+    
+    data = []
+    char_count = 0
+
+    # get info from input file
+    with open(input_file_string, "r") as input_file:
+        for line in input_file:     
+            for word in line.split():
+                data.append(word + " ") 
+                    
+                if newlines:
+                    char_count += len(word)
+                    # newlines every 100 char
+                    if char_count > 100:
+                        data.append("\n") 
+                        char_count = 0
+    
+    # reverse the array
+    reversed_data = data[::-1]
+    
+    # write the reversed list to the output file
+    output_file.writelines(reversed_data)
+    output_file.close()
+
+def reverse_text_with_punctuation(input_file_string, output_file_string, newlines=True):
+    """Takes text input file and creates a new file with all the text flipped, including punctuation.
+    For example, "Hi, I'm John" -> "John I'm ,Hi"
+
+    Args:
+        input_file_string (str): Path to input file
+        output_file_string (str): Path to the output file. 
+        newlines (bool, optional): Whether or not you want the output file to have new lines. Defaults to True.
+    """
+    output_file = open(output_file_string, "w")
+    
+    data = []
+    char_count = 0
+
+    # get info from input file
+    with open(input_file_string, "r") as input_file:
+        for line in input_file:     
+            for word in line.split():
+                if word[-1] in string.punctuation:
+                    # if the word has punctuation attached to the end, seperate them and add the word first
+                    data.append(word[:-1] + " ")
+                    data.append(word[-1])
+                else:           
+                    data.append(word + " ")
+                    
+                if newlines:
+                    char_count += len(word)
+                    # newlines every 100 char
+                    if char_count > 100:
+                        data.append("\n") 
+                        char_count = 0
+    
+    # reverse the array
+    reversed_data = data[::-1]
+    
+    # write the reversed list to the output file
+    output_file.writelines(reversed_data)
+    output_file.close()
+    
+def main():
+    reverse_text("data/input.txt", "data/output.txt")
+    reverse_text_with_punctuation("data/input.txt", "data/punc_output.txt")
+
+if __name__ == "__main__":
+    main()
